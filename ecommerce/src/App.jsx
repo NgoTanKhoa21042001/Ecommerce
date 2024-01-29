80fc5296aaefdfed7b32da1e24de37e4fcda58d5
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2024-01-29 17:33:03 +0700

    Animated Banner Section with Tailwind CSS

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -6,7 +6,32 @@ import Hero from "./components/Hero/Hero";
 import Category from "./components/Category/Category";
 import Category2 from "./components/Category/Category2";
 import Services from "./components/Services/Services";
+import headphone from "./assets/hero/headphone.png";
+import smartwatch2 from "./assets/category/smartwatch2-removebg-preview.png";
 
+import Banner from "./components/Banner/Banner";
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#f42c37",
+};
+const BannerData2 = {
+  discount: "30% OFF",
+  title: "Happy Hours",
+  date: "14 Jan to 28 Jan",
+  image: smartwatch2,
+  title2: "Smart Solo",
+  title3: "Winter Sale",
+  title4:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#2dcc6f",
+};
 function App() {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
@@ -15,6 +40,7 @@ function App() {
       <Category />
       <Category2 />
       <Services />
+      <Banner data={BannerData} />
     </div>
   );
 }
