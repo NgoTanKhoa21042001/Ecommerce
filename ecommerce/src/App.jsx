5189454b4e1f65a7badf22db1260bbe00ce45d94
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2024-01-29 15:59:09 +0700

    Category Card Section

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -2,11 +2,19 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Category from "./components/Category/Category";
+import Category2 from "./components/Category/Category2";
+import Services from "./components/Services/Services";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Category />
+      <Category2 />
+      <Services />
     </>
   );
 }
